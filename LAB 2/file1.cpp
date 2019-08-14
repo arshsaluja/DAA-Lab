f0db0a9d0a5d2009f0b387bac641b0d7ccb8b992
Kshitiz <kshitizsaini.rtk@gmail.com>
2019-08-14 09:43:49 +0530

    Program of lab

--- LAB 2/file1.cpp
+++ LAB 2/file1.cpp
@@ -4,25 +4,46 @@ using namespace std;
 
 int main()
 {
-  int array[10];
+  static int array[10];
   int position,number;
+  int temp;
+  static int count;
 
   for(int i=0;i<10;i++)
   {
-    cout<<"Enter the element you want to enter : ";
+    cout<<"Enter the element you want to enter (except 0) : ";
     cin>>number;
     cout<<"Enter the position of element : ";
     cin>>position;
-
     array[position]=number;
-
   }
 
-  cout<<"Array is : ";
+  for(int i=0;i<10;i++)
+  {
+    for(int j=i+1;j<10;j++)
+    {
+      if(array[i]>array[j])
+      {
+        temp=array[i];
+        array[i]=array[j];
+        array[j]=temp;
+      }
+    }
+  }
+
+  cout<<"Sorted array : ";
   for(int j=0;j<10;j++)
   {
-    cout<<array[j]<<" , ";
+    if(array[j]!=0)
+    {
+      cout<<array[j]<<" , ";
+      count++;
+    }
   }
+
+int *arr = new int[count];
+
+
 
   return 0;
 }
