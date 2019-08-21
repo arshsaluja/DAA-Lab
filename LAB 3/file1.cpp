43173596f16404e341b43b90a276ddd3617cd5f6
Kshitiz <kshitizsaini.rtk@gmail.com>
2019-08-21 09:15:33 +0530

    code modified

--- LAB 3/file1.cpp
+++ LAB 3/file1.cpp
@@ -10,10 +10,20 @@ int main()
   cin>>n;
   int *arr = new int[n];
 
+  cout<<endl<<endl<<endl;
+
   cout<<"Enter "<<n<<" elements for array : ";
   for(int i=0;i<n;i++)
   {
     cin>>arr[i];
+  }
+
+  cout<<endl<<endl<<endl;
+
+  cout<<"Entered array is : ";
+  for(int i=0;i<n;i++)
+  {
+    cout<<arr[i]<<", ";
   }
 
   return 0;
