bd11abb7f226b7df347a0b0da5e2847dedd73ca5
Kshitiz <kshitizsaini.rtk@gmail.com>
2019-08-21 08:58:09 +0530

    Elements entered

--- LAB 3/file1.cpp
+++ LAB 3/file1.cpp
@@ -2,7 +2,19 @@
 
 using namespace std;
 
+
 int main()
 {
+  int n;
+  cout<<"Enter number of elements in the array : ";
+  cin>>n;
+  int *arr = new int[n];
+
+  cout<<"Enter "<<n<<" elements for array : ";
+  for(int i=0;i<n;i++)
+  {
+    cin>>arr[i];
+  }
+
   return 0;
 }
