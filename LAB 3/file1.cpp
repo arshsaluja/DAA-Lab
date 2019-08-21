bdda25ea7bbeeb82865bad90bd9f472ed38151e9
Kshitiz <kshitizsaini.rtk@gmail.com>
2019-08-21 09:15:39 +0530

    code completed

--- LAB 3/file1.cpp
+++ LAB 3/file1.cpp
@@ -6,6 +6,8 @@ using namespace std;
 int main()
 {
   int n;
+  int location;
+
   cout<<"Enter number of elements in the array : ";
   cin>>n;
   int *arr = new int[n];
@@ -25,6 +27,32 @@ int main()
   {
     cout<<arr[i]<<", ";
   }
+
+  cout<<endl<<endl<<endl;
+
+  cout<<"Enter location of element to be deleted (starting with ) : ";
+  cin>>location;
+  if(location>n)
+  {
+    cout<<"Deletion not possible";
+  }
+  else
+  {
+    for(int i=location-1;i<n-1;i++)
+    {
+      arr[i]=arr[i+1];
+    }
+    n--;
+  }
+
+  cout<<endl<<endl<<endl;
+
+  cout<<"Entered array is : ";
+  for(int i=0;i<n;i++)
+  {
+    cout<<arr[i]<<", ";
+  }
+
 
   return 0;
 }
